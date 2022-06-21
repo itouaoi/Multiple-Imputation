366955d6722d738265cedf191f2abd9a2d02e11a
Aoi Ito <82706937+itouaoi@users.noreply.github.com>
2022-06-21 15:34:09 +0900

    Update mi_cross.R

--- mi_cross.R
+++ mi_cross.R
@@ -1,22 +1,65 @@
-#Multiple Imputation for Cross-Sectional Data
-install.packages("mice"); install.packages("broom.mixed")
-require(mice); require(broom.mixed)
+# Multiple Imputation with mice 
+install.packages("mice")
+library(mice)
 
-#1.Check the data distributions and pattern
+# Basic workflow with mice
+imp <- mice(nhanes)                   #Imputation
+fit <- with(imp, lm(chl ~ bmi + age)) #Analysis
+res <- pool(fit)                      #Pooling
+res
+summary(res)
+
+##################################### 1. Multiple Imputation single level ###################################
+# Check the data distributions and pattern
 data <- nhanes2; attach(data); summary(data)
-par(mfrow = c(1,2)); hist(bmi); hist(chl)
-md.pattern(data)
+par(mfrow = c(2,2)); hist(bmi); hist(chl); plot(age, main="age"); plot(hyp, main="hyp")
+par(mfrow = c(1,1));md.pattern(data)
 
-#2-1.Imputation with default setting (m=5)
-imp_data1 <- mice(data=data)
 
-#2-2.Plot
-plot(imp_data1)
-densityplot(imp_data1)
+# 1-1.Imputation with default setting (m=5)
+imp1 <- mice(data=data, seed = 1234)
 
-#3-1.Imputation with original setting
-imp_data2 <- mice(data=data, m=20, maxit=5, meth=c("", "pmm", "logreg", "pmm"))
+# 1-2.Plot 
+plot(imp1)
+densityplot(imp1)
+stripplot(imp1, pch = 19, xlab = "Imputation number")
 
-#3-2.Plot
-plot(imp_data2)
-densityplot(imp_data2)
+# 2. Analysis
+fit1 <- with(imp1, lm(chl ~ bmi + age)) 
+
+# 3. Pooling the results
+res1 <- pool(fit1)
+res1
+summary(res1)
+summary(res1, "all", conf.int = TRUE)
+
+
+# 4-1.Imputation with predictive mean matching
+require(mice); data <- nhanes2; attach(data)
+head(data)
+imp2 <- mice(data = data, seed = 1234, m=5, maxit=5,
+             method=c("", "pmm", "logreg", "pmm"))
+
+stripplot(imp2, pch = 19, xlab = "Imputation number
+          Predictive mean matching (pmm)")
+
+
+# 4-2.Imputation with Bayesian linear regression 
+imp3 <- mice(data = data, seed = 1234, m=5, maxit=5,
+             method=c("", "norm", "logreg", "norm"))
+
+stripplot(imp3, pch = 19, xlab = "Imputation number
+         Bayesian linear regression (norm)")
+
+# 5-1.Predictor Matrix
+imp <- mice(data = data, seed = 1234, print = FALSE)
+imp$pred
+
+# 5-2. Make a original predictor matrix for big data set.
+pred <- imp$pred
+write.csv(pred, file = "test.csv")
+pred_2 = read.csv("test.csv", header = T, row.names = 1)
+pred_2 <- as.matrix(pred_2); pred_2
+
+imp4 <- mice(data = data, seed = 1234, print = FALSE,
+             pred = pred_2)
